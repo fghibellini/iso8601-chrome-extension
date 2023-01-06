07c5c05c052e5f9f1c7dd7c809a475082e54fc64
Filippo Ghibellini <f.ghibellini@gmail.com>
2023-01-06 16:18:08 +0100

    Date extension

--- src/content.js
+++ src/content.js
@@ -8,31 +8,29 @@
 
 import { init } from './framework';
 
-// https://stackoverflow.com/a/6640851
-const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+// https://stackoverflow.com/a/14322189
+const iso8601Regex = /([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?/
 
-function uuidMatchingFn(node) {
+
+function iso8601MatchingFn(node) {
   // ignore very big text nodes
   // (we are periodically iterating over the whole document)
   if (node.data.length > 1000) {
     return false;
   } else {
     // we don't perform an exact match (/^...$/) since we want to be able
-    // to annotate even text that might INCLUDE a UUID (e.g. per-entity resources)
-    const uuidsInNode = node.data.match(new RegExp(uuidRegex, 'g')) || []
-    // more than 1 match not interesting as it wouldn't be clear which UUID the tooltip is for
+    // to annotate even text that might INCLUDE a date (e.g. per-entity resources)
+    const datesInNode = node.data.match(new RegExp(iso8601Regex, 'g')) || []
+    // more than 1 match not interesting as it wouldn't be clear which date the tooltip is for
     // and splitting text nodes is beyond the scope for now
-    return (uuidsInNode.length === 1) ? uuidsInNode : [];
+    return (datesInNode.length === 1) ? datesInNode : [];
   }
 }
 
-async function uuidResolveFn(uuid) {
-  const knownUUIDs = [
-    { uuid: '7a18a230-f4d0-4fbb-b706-0a16479409a5', html: `The best <b>UUID</b> ever!` }
-  ]
-  const matchingResult = knownUUIDs.find(x => x.uuid === uuid);
-  const html = matchingResult ? matchingResult.html : `<i>UNKNOWN UUID</i>`;
-  await randomDelay(200, 2000);
+async function uuidResolveFn(strDate) {
+  const matchingResult = new Date(strDate);
+  const html = matchingResult ? matchingResult.toLocaleDateString() : `<i>UNKNOWN UUID</i>`;
+  await randomDelay(200, 300);
   return html;
 }
 
